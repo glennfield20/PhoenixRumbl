3cbaed848fb17bcb44a0aed539be1f9a74194b9d
glennfield20 <ladesmaglenngarfield@gmail.com>
2018-02-12 16:49:52 +0800

    Authentication with Plug, Restricting Access.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,12 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show] 
   alias Rumbl.User
+
+  def index(conn, _params) do
+    users = Repo.all(Rumbl.User)
+    render conn, "index.html", users: users
+  end
 
   # changeset = receives struct and controller parameter and returns Ecto.Changeset.
   # Manage record changes, cast parameters and perform validations.
@@ -10,7 +16,7 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
       {:ok, user} -> conn
         |> put_flash(:info, "#{user.name} created!")
@@ -20,13 +26,19 @@ defmodule Rumbl.UserController do
     end
   end
 
-  def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
-    render conn, "index.html", users: users
-  end
-
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt() # Halt is to stop any downstream transformations.
+    end
   end
 end
